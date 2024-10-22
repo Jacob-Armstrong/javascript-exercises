7c779f845928f6d4b39c3589ff2c50afbae5d720
Jacob <29027100+Jacob-Armstrong@users.noreply.github.com>
2024-10-22 15:33:57 -0700

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    if (start > end) {
+        let temp = 0;
+        temp = start;
+        start = end;
+        end = temp;
+    }
+
+    if ((start < 1 || end < 1) || !Number.isInteger(start) || !Number.isInteger(end) ){
+        return "ERROR"
+    }
+
+    let sum = 0;
+
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
