dc7aee930fa2512337bb3e45b1a95eb99159add4
Jacob <29027100+Jacob-Armstrong@users.noreply.github.com>
2024-10-22 15:06:08 -0700

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    newStr = ""
 
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < num; i++) {
+        newStr += str
+    }
+
+    return newStr
 };
 
 // Do not edit below this line
