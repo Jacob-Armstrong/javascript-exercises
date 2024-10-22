03dfc96646a93ce8e31736a8f6a106849f5adf7c
Jacob <29027100+Jacob-Armstrong@users.noreply.github.com>
2024-10-22 15:37:11 -0700

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let leapYear = false;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    return leapYear;
 
 };
 
